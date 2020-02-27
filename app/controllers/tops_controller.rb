e5dd91f2705c1ce1399806f93c05acfb3c9eb4c6
takadamu <0rg68v3m326626f@gmail.com>
2020-02-27 20:32:07 +0900

    bar-lists-layout

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,8 @@ class TopsController < ApplicationController
 
   def index
     @posts = Post.all
+    @search = Post.ransack(params[:q])
+    @results = @search.result.order("created_at DESC")
   end
 
 
