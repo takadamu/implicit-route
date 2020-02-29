b9dcfc0d5dc57390d30baf0b1c29823055bc3d18
takadamu <0rg68v3m326626f@gmail.com>
2020-02-29 15:23:57 +0900

    map-geocoding

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -60,3 +60,42 @@ google.maps.event.addListener(myInfoWindow, "closeclick", function() {
     myInfoWindow.open(map, marker);
   });
 });
+
+var map;
+function initialize() {
+  // 地図を表示する際のオプションを設定
+
+
+  // Mapオブジェクトに地図表示要素情報とオプション情報を渡し、インスタンス生成
+  map = new google.maps.Map(document.getElementById("map"),
+      mapOptions);
+}
+ 
+function search(){
+   var place = document.getElementById('place').value;
+   var geocoder = new google.maps.Geocoder();
+   // ジオコーディング　検索実行
+   geocoder.geocode({"address" : place}, function(results, status) {
+   if (status == google.maps.GeocoderStatus.OK) {
+         
+        var lat = results[0].geometry.location.lat();//緯度を取得
+        var lng = results[0].geometry.location.lng();//経度を取得
+        var ido = document.getElementById('ido')
+        ido.innerHTML = lat;
+        var keido = document.getElementById('keido')
+        keido.innerHTML = lng;
+        var mark = {
+            lat: lat, // 緯度
+            lng: lng // 経度
+        };
+        marker = new google.maps.Marker({ // マーカーの追加
+        position: mark, // マーカーを立てる位置を指定
+        map: map // マーカーを立てる地図を指定
+        });
+         
+        map.setCenter(results[0].geometry.location); // 地図の中心を移動
+        cnt =0;
+   }
+   });
+   //}
+}
