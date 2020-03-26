0795a47a4b6adcf0e61846d50210dabfb3df6d49
takadamu <0rg68v3m326626f@gmail.com>
2020-03-26 18:30:21 +0900

    js実行タイミング変更（by中島）

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,51 +14,53 @@
 //= require activestorage
 //= require_tree .
 
+window.onload = function() {
+  // 初期設定
+  var MyLatLng = new google.maps.LatLng(37.916194,139.036389);
+  var Options = {
+  zoom: 6,      //地図の縮尺値
+  center: MyLatLng,    //地図の中心座標
+  mapTypeId: 'roadmap'   //地図の種類
+  };
+  var map = new google.maps.Map(document.getElementById('map'), Options);
 
-// 初期設定
-var MyLatLng = new google.maps.LatLng(37.916194,139.036389);
-var Options = {
-zoom: 6,      //地図の縮尺値
-center: MyLatLng,    //地図の中心座標
-mapTypeId: 'roadmap'   //地図の種類
-};
-var map = new google.maps.Map(document.getElementById('map'), Options);
-
-// じゃんけんトンネルのマーク
- var marker = new google.maps.Marker({
-  position: new google.maps.LatLng(35.823665, 139.920940),
-  map: map,
- });
- var myInfoWindow = new google.maps.InfoWindow({
-  // 吹き出しに出す文
-  content: "じゃんけんトンネル"
-});
-// 吹き出しを開く
-myInfoWindow.open(map, marker);
-// 吹き出しが閉じられたら、マーカークリックで再び開くようにしておく
-google.maps.event.addListener(myInfoWindow, "closeclick", function() {
-  google.maps.event.addListenerOnce(marker, "click", function(event) {
-    myInfoWindow.open(map, marker);
+  // じゃんけんトンネルのマーク
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(35.823665, 139.920940),
+    map: map,
   });
-});
-
-// 基幹バスレーンのマーク
-var marker = new google.maps.Marker({
-  position: new google.maps.LatLng(35.182952, 136.954826),
-  map: map,
- });
- var myInfoWindow = new google.maps.InfoWindow({
-  // 吹き出しに出す文
-  content: "魔の道路"
-});
-// 吹き出しを開く
-myInfoWindow.open(map, marker);
-// 吹き出しが閉じられたら、マーカークリックで再び開くようにしておく
-google.maps.event.addListener(myInfoWindow, "closeclick", function() {
-  google.maps.event.addListenerOnce(marker, "click", function(event) {
-    myInfoWindow.open(map, marker);
+  var myInfoWindow = new google.maps.InfoWindow({
+    // 吹き出しに出す文
+    content: "じゃんけんトンネル"
   });
-});
+  // 吹き出しを開く
+  myInfoWindow.open(map, marker);
+  // 吹き出しが閉じられたら、マーカークリックで再び開くようにしておく
+  google.maps.event.addListener(myInfoWindow, "closeclick", function() {
+    google.maps.event.addListenerOnce(marker, "click", function(event) {
+      myInfoWindow.open(map, marker);
+    });
+  });
+
+  // 基幹バスレーンのマーク
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(35.182952, 136.954826),
+    map: map,
+  });
+  var myInfoWindow = new google.maps.InfoWindow({
+    // 吹き出しに出す文
+    content: "魔の道路"
+  });
+  // 吹き出しを開く
+  myInfoWindow.open(map, marker);
+  // 吹き出しが閉じられたら、マーカークリックで再び開くようにしておく
+  google.maps.event.addListener(myInfoWindow, "closeclick", function() {
+    google.maps.event.addListenerOnce(marker, "click", function(event) {
+      myInfoWindow.open(map, marker);
+    });
+  });
+}
+
 
 var map;
 function initialize() {
